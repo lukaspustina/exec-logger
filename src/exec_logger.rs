3d95a041fd24ce5650653c0a1bf299b6877be1f3
Lukas Pustina <lukas@pustina.de>
2020-06-20 19:24:55 +0200

    Add Output interface

--- src/exec_logger.rs
+++ src/exec_logger.rs
@@ -1,5 +1,6 @@
 use crate::bpf;
 use failure::Error;
+use std::collections::HashMap;
 use std::sync::{atomic::AtomicBool, Arc, Mutex};
 
 #[derive(Debug)]
@@ -10,6 +11,7 @@ enum Event {
 
 #[derive(Debug)]
 struct Arg {
+    pid: i32,
     argv: String,
 }
 
@@ -24,6 +26,7 @@ impl From<bpf::Event> for Event {
         match event.r#type {
             bpf::EventType::EVENT_ARG => {
                 Event::Arg(Arg {
+                    pid:  event.pid,
                     argv: bpf::parse_string(&event.argv),
                 })
             }
@@ -42,19 +45,60 @@ pub struct ExecLogger {
     args:     ExecLoggerArgs,
 }
 
+trait Output {
+    fn header(&self) -> Result<(), Error>;
+    fn arg(&self, arg: Arg) -> Result<(), Error>;
+    fn ret(&self, ret: Return) -> Result<(), Error>;
+}
+
+struct SimpleOutput {
+    args: Arc<Mutex<HashMap<i32, Vec<String>>>>,
+}
+
+impl SimpleOutput {
+    fn new() -> Self { 
+        SimpleOutput {
+            args: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
+
+impl Output for SimpleOutput {
+    fn header(&self) -> Result<(), Error> {
+        println!("{:-16} {:-7}", "PCOMM", "PID");
+        Ok(())
+
+    }
+
+    fn arg(&self, arg: Arg) -> Result<(), Error> {
+        let mut args = self.args.lock().unwrap();
+        let value = args.entry(arg.pid).or_insert_with(|| Vec::new());
+        value.push(arg.argv);
+        Ok(())
+    }
+
+    fn ret(&self, ret: Return) -> Result<(), Error> {
+        let mut args = self.args.lock().unwrap();
+        let value = args.remove(&ret.pid);
+        println!("{:?}: {:?}", ret, value);
+        Ok(())
+    }
+}
+
 impl ExecLogger {
     pub fn new(runnable: Arc<AtomicBool>, args: ExecLoggerArgs) -> Self { ExecLogger { runnable, args } }
 
     pub fn run(self) -> Result<(), Error> {
-        let v = Arc::new(Mutex::new(Vec::new()));
+        let output = SimpleOutput::new();
+        output.header()?;
+        let output = Arc::new(Mutex::new(output));
 
         let handler = move |event: bpf::Event| {
             let event: Event = event.into();
-            let mut v = v.lock().unwrap();
-            v.push(1);
+            let output = output.lock().unwrap();
             match event {
-                Event::Arg(a) => println!("Arg: {}", a.argv),
-                Event::Return(r) => println!("{:-16} {:-7}", r.comm, r.pid),
+                Event::Arg(a) => output.arg(a).unwrap(),
+                Event::Return(r) => output.ret(r).unwrap(),
             }
         };
 
@@ -62,7 +106,7 @@ impl ExecLogger {
             max_args: self.args.max_args,
         };
         let kprobe = bpf::KProbe::new(self.runnable, handler, kprobe_args);
-        println!("{:-16} {:-7}", "PCOMM", "PID");
+
         kprobe.run()
     }
 }
